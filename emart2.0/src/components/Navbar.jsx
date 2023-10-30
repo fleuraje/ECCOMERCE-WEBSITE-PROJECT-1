d2f497ebd215ea6ff5a686148db2a83a467e8046
fleuraje <127205495+fleuraje@users.noreply.github.com>
2023-10-30 19:05:19 -0400

    added product description and price in product cart page

--- emart2.0/src/components/Navbar.jsx
+++ emart2.0/src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
                <NavLink to="/register" className="btn btn-outline-dark ms-2">
                <i className="fa fa-user-plus me-1"></i> Register</NavLink>
                <NavLink to="/cart" className="btn btn-outline-dark ms-2">
-               <i className="fa fa-shopping-cart me-1"></i> Cart {cartItems.length}</NavLink>
+               <i className="fa fa-shopping-cart me-1"></i> Cart (0)</NavLink>
            </div>
           </div>
         </div>
--- emart2.0/src/components/Product.jsx
+++ emart2.0/src/components/Product.jsx
@@ -1,66 +1,60 @@
-import React, {useState, useEffect, useContext} from 'react';
-import { useParams } from 'react-router';
-import { CartContext } from '../context/cart';
-
-
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router";
+import { CartContext } from "../context/cart";
 
 const Product = () => {
-    
-        const { cartItems, addToCart } = useContext(CartContext)
-    
+  const { cartItems, addToCart } = useContext(CartContext);
 
-    const {id} = useParams();
-    const [product, setProduct] = useState([]);
-    const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const getProduct = async () => {
-            setLoading(true);
-            const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-            setProduct(await response.json());
-            setLoading(false);
-            
-        }
-        getProduct();
-        
-    }, []);
-    const Loading = () => {
-        return(
-            <>
-            Loading...
-            </>
-        )
-
-    }
-    const ShowProduct = () => {
-        return(
-            <>
-            <div className="col-md-6">
-                <img src={product.image} alt={product.title}
-                height="400px" width="400px" />
-            </div>
-            <div className="col-md-6">
-                <h4 className='text-uppercase text-black-50'>
-                {product.category}
-                </h4>
-                <button onClick={() => addToCart(product)} className='px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700'>Add to cart</button>
-                <p class="card-text" >
-                      ${product.price}
-                    </p>
-            </div>
-            </>
-
-        )
-    }
+  useEffect(() => {
+    const getProduct = async () => {
+      setLoading(true);
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      setProduct(await response.json());
+      setLoading(false);
+    };
+    getProduct();
+  }, []);
+  const Loading = () => {
+    return <>Loading...</>;
+  };
+  const ShowProduct = () => {
     return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    {loading ? <Loading/> : <ShowProduct/>}
-                </div>
-            </div>
+      <>
+        <div className="col-md-6">
+          <img
+            src={product.image}
+            alt={product.title}
+            height="400px"
+            width="400px"
+            style={{ marginTop: "20px" }}
+          />
         </div>
-    )
-}
+
+        <div className="col-md-6">
+          <h4 className="text-uppercase text-black-50">{product.category}</h4>
+          <h4 className="card-text">${product.price}</h4>
+          <p>{product.description}</p>
+          <button
+            onClick={() => addToCart(product)}
+            className="px-4 py-2 bg-black text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+          >
+            Add to Cart
+          </button>
+        </div>
+      </>
+    );
+  };
+  return (
+    <div>
+      <div className="container">
+        <div className="row">{loading ? <Loading /> : <ShowProduct />}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Product;
